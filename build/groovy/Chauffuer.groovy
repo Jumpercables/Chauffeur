81463a354a9dee572ead683f46b634289aab3c21
Jumpercables <kyle.baesler@gmail.com>
2015-08-20 17:14:51 -0700

    Converted to script format.

--- build/groovy/Chauffuer.groovy
+++ build/groovy/Chauffuer.groovy
@@ -4,43 +4,37 @@
         machines (that are hosting the Jenkins Chauffeur Service) that a new
         build should be installed.
     .NOTES
-        File Name      : chauffeur.groovy
+        File Name      : Chauffeur.groovy
         Author         : Kyle Baesler
         Prerequisite   : Groovy 1.8.9
     .EXAMPLE
-        chauffeur.groovy
+        Chauffeur.groovy
  */
 
-class chauffeur {
+// The name of the job in the build environment.
+def JOB_NAME = ""
 
-    // The name of the job in the build environment.
-    static final def JOB_NAME = ""
+// The name of the computers that host the Chauffeur service.
+def MACHINE_NAMES = []
 
-    // The name of the computers that host the Chauffeur service.
-    static final def MACHINE_NAMES = []
+// The name of the job in the build.
+if (JOB_NAME == "" || JOB_NAME == null) {
+    println("The <JOB_NAME> must be specified.")
+    return
+}
 
-    static void main(String... args) {
+// The name of the machines that should be notified.
+if (MACHINE_NAMES == null || MACHINE_NAMES.size() == 0) {
+    println("The <MACHINE_NAMES> must be specified.")
+    return
+}
 
-        // The name of the job in the build.
-        if(JOB_NAME == "" || JOB_NAME == null) {
-            println("The <JOB_NAME> must be specified.")
-            return
-        }
-
-        // The name of the machines that should be notified.
-        if(MACHINE_NAMES == null || MACHINE_NAMES.size() == 0) {
-            println("The <MACHINE_NAMES> must be specified.")
-            return
-        }
-
-        try {
-            MACHINE_NAMES.eachWithIndex { String s, int i ->
-                def url = new URL('http://' + s + ':8080/Chauffeur.Jenkins.Services/ChauffeurService/rest/InstallLastSuccessfulBuild/' + JOB_NAME)
-                def text = url.getText()
-                println(text)
-            }
-        } catch (Exception e) {
-            e.printStackTrace()
-        }
+try {
+    MACHINE_NAMES.eachWithIndex { String s, int i ->
+        def url = new URL('http://' + s + ':8080/Chauffeur.Jenkins.Services/ChauffeurService/rest/InstallLastSuccessfulBuild/' + JOB_NAME)
+        def text = url.getText()
+        println(text)
     }
+} catch (Exception e) {
+    println(e.printStackTrace())
 }
